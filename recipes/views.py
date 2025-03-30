33372f7ffd9e3fcca646edc88e7c3421fa61c27a
Gabryel Duarte <gabryelduartepessanha@gmail.com>
2025-03-30 17:54:27 -0300

    inicio

--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,6 @@
 from django.shortcuts import render
-from django.shortcuts import HttpResponse
 
 def home(request):
-    # return HttpResponse("Home")
-    return render(request, 'home.html')
-
-
-def contato(request):
-    return HttpResponse("Contato")
-
-def sobre(request):
-    return HttpResponse("Sobre")
+    return render(request, 'recipes/pages/home.html', context={
+        'name' : 'Gabryel Duarte',
+    })
